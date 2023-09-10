088cac3f59c0075d7bf34096cb090afb4e6c9ee3
William Chandra <william.chandra@pixel8labs.com>
2023-09-10 10:25:21 +0700

    fix: add response and request log middleware

--- middleware/log.go
+++ middleware/log.go
@@ -1,41 +1,90 @@
-package middleware
+package restmiddleware
 
 import (
-	"time"
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
-	"go.opentelemetry.io/otel/trace"
+	"github.com/pixel8labs/logtrace/log"
 )
 
-func TracingLogMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		ctx := c.Request().Context()
-		start := time.Now()
-		err := next(c)
-		traceID := trace.SpanFromContext(ctx).SpanContext().TraceID().String()
-		stop := time.Now()
+type customContext struct {
+	echo.Context
+}
 
-		var errorMsg string
-		if err != nil {
-			errorMsg = err.Error()
+// JSON override echo's c.JSON
+func (c customContext) JSON(code int, payload interface{}) error {
+	request := c.Request()
+	err := c.Context.JSON(code, payload)
+
+	// Log outgoing response
+	resCtx := map[string]interface{}{
+		"status":  c.Response().Status,
+		"headers": c.Response().Header(),
+		"body":    payload,
+	}
+
+	log.Info(c.Request().Context(), log.Fields{
+		"request":  c.Get("request"),
+		"response": resCtx,
+	}, "Outgoing response: %s %s",
+		request.Method,
+		request.RequestURI,
+	)
+
+	return err
+}
+
+func getObject(rawData []byte) (interface{}, bool) {
+	var object interface{}
+	err := json.Unmarshal(rawData, &object)
+	if err != nil {
+		return nil, false
+	}
+	return object, true
+}
+
+func getRequestBody(req *http.Request) (interface{}, string, bool) {
+	var reqBody []byte
+	if req.Body != nil {
+		reqBody, _ = io.ReadAll(req.Body)
+	}
+	req.Body = io.NopCloser(bytes.NewBuffer(reqBody)) // Reset
+
+	object, ok := getObject(reqBody)
+	return object, string(reqBody), ok
+}
+
+func Logger() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			request := c.Request()
+
+			// Log incoming request.
+			reqCtx := map[string]interface{}{
+				"method":  request.Method,
+				"url":     request.RequestURI,
+				"query":   c.QueryParams(),
+				"headers": request.Header,
+			}
+			if body, rawString, ok := getRequestBody(request); ok {
+				reqCtx["body"] = body
+			} else {
+				reqCtx["body"] = rawString
+			}
+
+			log.Info(request.Context(), log.Fields{
+				"request": reqCtx,
+			}, "Incoming request: %s %s",
+				request.Method,
+				request.RequestURI,
+			)
+
+			c.Set("request", reqCtx)
+
+			return next(customContext{c})
 		}
-
-		c.Logger().Infoj(map[string]interface{}{
-			"time":       stop.Format(time.RFC3339),
-			"trace_id":   traceID,
-			"id":         c.Response().Header().Get(echo.HeaderXRequestID),
-			"remote_ip":  c.RealIP(),
-			"host":       c.Request().Host,
-			"method":     c.Request().Method,
-			"uri":        c.Request().RequestURI,
-			"user_agent": c.Request().UserAgent(),
-			"status":     c.Response().Status,
-			"error":      errorMsg,
-			"latency":    stop.Sub(start).Seconds(),
-			"bytes_in":   c.Request().ContentLength,
-			"bytes_out":  c.Response().Size,
-		})
-
-		return err
 	}
 }
